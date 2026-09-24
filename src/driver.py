19bd3a57a8945e4989fd0e0390709976324619d2
Jędrzej Majchrzak <jedmajc@gmail.com>
2026-09-25 00:08:27 +0200

    har coded rozmiar okna for  consistency wyszukanie i scrollowanie do zestawu dziennego prymityw byleby dzialal

--- src/driver.py
+++ src/driver.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from constants import USER_DATA_DIR, PROFILE_NAME
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 options = webdriver.EdgeOptions()
 
@@ -12,7 +15,21 @@ options.add_argument(f"--profile-directory={PROFILE_NAME}")
 
 driver = webdriver.Edge(options=options)
 
-driver.get("https://bing.com")
+driver.set_window_size(1920, 1080)
+driver.get("https://rewards.bing.com/dashboard")
+
+driver.implicitly_wait(5)
+
+element = driver.find_element(
+    By.XPATH,
+    f"//*[contains(text(), 'Twoja aktywność')]"
+)
+
+actions = ActionChains(driver)
+actions.move_to_element(element).perform()
+
+print(element.location)
+
 
 input("Press Enter to close the browser")
 
