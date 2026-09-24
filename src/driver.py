da205abdd30ccc83a4435864aa0c4a441ede70df
Jędrzej Majchrzak <jedmajc@gmail.com>
2026-09-24 22:52:42 +0200

    Pierwszy krok: przegladarka sie otwiera 9i nie wykrywa bot control)

---
+++ src/driver.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from constants import USER_DATA_DIR, PROFILE_NAME
+
+
+options = webdriver.EdgeOptions()
+
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option('useAutomationExtension', False)
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument(f"--user-data-dir={USER_DATA_DIR}")
+options.add_argument(f"--profile-directory={PROFILE_NAME}")
+
+driver = webdriver.Edge(options=options)
+
+driver.get("https://bing.com")
+
+input("Press Enter to close the browser")
+
+driver.quit()
